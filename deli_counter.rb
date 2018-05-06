7fdc161d46ecd3986f405294ce9303e289e9ef63
Elana Ryznar <eryzna@gmail.com>
2018-05-06 22:49:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,11 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.each.with_index {|customer, index| puts "The line is currently: #{index+1}. #{customer}."}
+    current_line= "The line is currently:"
+    katz_deli.each.with_index do |person, index|
+      current_line << "#{index+1}. #{person}"
+    end
+    puts current_line
   end
 end
 
@@ -17,3 +21,5 @@ end
 #    puts current_line
 #  end
 #end
+
+#E katz_deli.each.with_index {|customer, index| puts "The line is currently: #{index+1}. #{customer}."}
