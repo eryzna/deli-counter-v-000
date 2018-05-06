0d4039bdc0787ffa6116c904f1cebcbd63df0fb7
Elana Ryznar <eryzna@gmail.com>
2018-05-06 22:59:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,8 @@ def line(katz_deli)
 end
 
 def take_a_number (katz_deli, name)
+  if !katz_deli.empty?
+    katz_deli << name
   current_position=katz_deli.length+1
   puts "Welcome, #{name}. You are number #{current_position} in line."
 end
