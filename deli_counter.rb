1f22e5b6db1f0eb928aa3cc4ca346174a4f69118
Elana Ryznar <eryzna@gmail.com>
2018-05-06 22:43:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,9 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index {|customer, index| katz_deli<< "The line is currently: #{index+1}. #{customer}."}
+    people_line=[]
+    katz_deli.each_with_index {|customer, index| people_line<< "The line is currently: #{index+1}. #{customer}."}
+    people_line
   end
 end
 
